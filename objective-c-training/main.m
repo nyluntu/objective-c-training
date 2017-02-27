af9a6ca7e0adeebe3ae5b44fac70584a8a6171f9
Turo Nylund <turo.nylund@outlook.com>
2017-02-27 10:00:29 +0200

    initial

---
+++ objective-c-training/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objective-c-training
+//
+//  Created by Sovelluskontti on 27/02/2017.
+//  Copyright © 2017 Sovelluskontti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
